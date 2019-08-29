2a8ef9af8c057a09378f71f2d6d6f1ab7920605f
Luis Fernando Raga Renteria <Luis Raga@DESARROLLO2.MODUARTSACI.LOCAL>
2019-08-29 14:11:54 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const SocketIO = require('socket.io')
+
+
+
+
+app.set('port', process.env.PORT || 3000)
+
+
+// Rutas
+// app.use('/', (req, res) => {
+//   res.status(200).send('La API funciona correctamente');
+// });
+
+//archivos estáticos
+console.log(path.join( __dirname,'publuc'));
+
+app.use(express.static(path.join( __dirname, 'public')))
+// Inicializar servidor
+const server = app.listen(app.get('port'), ()=>{
+  console.log('Servidor corriendo en el puerto '+app.get('port'))
+
+});
+
+const io = SocketIO.listen(server)
+
+// Web sockets
+
+io.on('connection', (socket)=>{
+  console.log('Usuario conectado '+socket.id);
+  socket.on('chat message', (e)=>{
+    console.log(e);
+
+    io.emit('chat message', e);
+    
+  })
+
+  // socket.emit('algo', socket)
+})
